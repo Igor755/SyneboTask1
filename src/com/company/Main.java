7b0b224d06df5e72647a36b1e0166611b43080bf
Jonny_B <metlin.igor@gmail.com>
2018-10-07 17:37:58 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter number of squares");
+        Scanner scan = new Scanner(System.in);
+
+
+        long n, k;
+        double length, width;
+
+
+        try {
+            n = scan.nextInt();
+
+
+            if (n >= 1 && n <= 109) {
+
+
+                length = Math.floor(Math.sqrt(n));
+                width = Math.ceil(n / length);
+                k = (long) (2 * n + length + width);
+                System.out.print("Number of matches = " + k);
+
+
+            } else {
+
+                System.out.println("n value not entered correctly");
+
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("You did not enter an integer");
+        }
+
+    }
+
+}
+
+
