17d48fac9edf247aecefcf8ce009c4b7f5ceebec
Jonny_B <metlin.igor@gmail.com>
2018-10-07 17:48:54 +0300

    Initial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Please enter number of squares");
+        System.out.println("Please enter number of squares (squares must be >= 1 and <= 109 and integer)");
         Scanner scan = new Scanner(System.in);
 
 
